2a2ffdc35d3093bddcae0ed69deb19433c130689
sivakumar6678 <chandragarisivakumar@gmail.com>
2024-12-01 15:11:42 +0530

    new code

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import FaceRecognition from './Components';
+
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Face Recognition App</h1>
+        <FaceRecognition />
+      </header>
+    </div>
+  )
+}
+
+export default App
